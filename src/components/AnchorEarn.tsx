b25ff5eb536bb220f672edf2732fa5e4b73db027
Sateesh Kodati <sateesh.kodati@gmail.com>
2022-02-25 03:12:35 -0800

    feat: anchor withdraw

--- src/components/AnchorEarn.tsx
+++ src/components/AnchorEarn.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Fee, Key, MnemonicKey, Tx, TxBody, Wallet} from '@terra-money/terra.js';
-import { useWallet, useConnectedWallet, useLCDClient } from '@terra-money/wallet-provider';
-import { Anchor, bombay12, AddressProviderFromJson, MARKET_DENOMS, fabricateMarketRedeemStable, fabricateMarketDepositStableCoin } from '@anchor-protocol/anchor.js'
-import { AnchorEarn, CHAINS, DENOMS  } from '@anchor-protocol/anchor-earn';
+import { Tx } from '@terra-money/terra.js';
+import {  useConnectedWallet, useLCDClient } from '@terra-money/wallet-provider';
+import { Anchor, bombay12, AddressProviderFromJson, MARKET_DENOMS } from '@anchor-protocol/anchor.js'
+
 
 
 
@@ -12,6 +12,7 @@ export function AnchorEarnApp() {
   
   const [totalDeposit, setTotalDeposit] = useState<any>(0);
   const [address, setAddress] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
 
   const addressProvider = new AddressProviderFromJson(bombay12);
   const anchor = new Anchor(lcd, addressProvider)
@@ -30,25 +31,53 @@ export function AnchorEarnApp() {
   }, [connectedWallet, lcd]);
 
   const deposit = async () =>{
+    setLoading(true);
     const depositMsg = anchor.earn.depositStable({amount: '10', market: MARKET_DENOMS.UUSD})
       .generateWithAddress(address)
 
-    const signResult = await connectedWallet?.sign({
-      msgs: depositMsg,
-    }) ;
+    const signResult = await connectedWallet?.sign({ msgs: depositMsg }) ;
     console.log(signResult?.result);
 
     const result = await lcd.tx.broadcast(signResult?.result as Tx);
     console.log(result);
-
+    anchor.earn.getTotalDeposit({address, market: MARKET_DENOMS.UUSD})
+    .then(depost => {
+        setTotalDeposit(depost);
+        setLoading(false);
+        // window.location.reload();
+    });
   };
   
+  const withdraw = async () => {
+    setLoading(true);
+    const withdrawMsg = anchor.earn.withdrawStable({ amount: '10', market: MARKET_DENOMS.UUSD})
+      .generateWithAddress(address);
+    const signResult = await connectedWallet?.sign({ msgs: withdrawMsg });
+    console.log(signResult?.result);
+
+    const result = await lcd.tx.broadcast(signResult?.result as Tx);
+    console.log(result);
+    anchor.earn.getTotalDeposit({address, market: MARKET_DENOMS.UUSD})
+    .then(depost => {
+        setTotalDeposit(depost);
+        setLoading(false);
+        // window.location.reload();
+    });
+  };
+
   return (
     <div>
       <h1>Anchor Earn</h1>
+      <div>
+        {loading && 
+          <b style={{ color: 'green' }}>Transaction In progress....</b>
+        }
+      </div>
       <b>Total Deposts:</b> {totalDeposit} UST
       <br />
-      <div><b>Depost</b> <button onClick={deposit}>Deposit</button></div>
+      <div><b>Depost 10 UST</b> <button onClick={deposit}>Deposit</button></div>
+      <br />
+      <div><b>Withdraw 10 UST</b> <button onClick={withdraw}>Withdraw</button></div>
     </div>
   );
 }
