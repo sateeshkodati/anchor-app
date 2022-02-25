87199736990219de60da35955962172fca056bfc
Sateesh Kodati <sateesh.kodati@gmail.com>
2022-02-25 02:53:39 -0800

    feat: deposit UST to anchor

--- src/components/AnchorEarn.tsx
+++ src/components/AnchorEarn.tsx
@@ -1,38 +1,54 @@
 import React, { useEffect, useState } from 'react';
-
-import { useConnectedWallet, useLCDClient } from '@terra-money/wallet-provider';
-import { Anchor, bombay12, AddressProviderFromJson, MARKET_DENOMS, OperationGasParameters } from '@anchor-protocol/anchor.js'
+import { Fee, Key, MnemonicKey, Tx, TxBody, Wallet} from '@terra-money/terra.js';
+import { useWallet, useConnectedWallet, useLCDClient } from '@terra-money/wallet-provider';
+import { Anchor, bombay12, AddressProviderFromJson, MARKET_DENOMS, fabricateMarketRedeemStable, fabricateMarketDepositStableCoin } from '@anchor-protocol/anchor.js'
+import { AnchorEarn, CHAINS, DENOMS  } from '@anchor-protocol/anchor-earn';
 
 
 
 export function AnchorEarnApp() {
   const lcd = useLCDClient();
   const connectedWallet = useConnectedWallet();
-
-  const [address, setAddress] = useState<string>();
+  
   const [totalDeposit, setTotalDeposit] = useState<any>(0);
+  const [address, setAddress] = useState<string>('');
+
+  const addressProvider = new AddressProviderFromJson(bombay12);
+  const anchor = new Anchor(lcd, addressProvider)
+
 
   useEffect(() => {
     if (connectedWallet) {
       setAddress(connectedWallet.walletAddress);
-
-      const addressProvider = new AddressProviderFromJson(bombay12);
-      const anchor = new Anchor(lcd, addressProvider)
-      
       anchor.earn.getTotalDeposit({address: connectedWallet.walletAddress, market: MARKET_DENOMS.UUSD})
         .then(depost => {
-            setTotalDeposit(depost)   
+            setTotalDeposit(depost);
         });
     } else {
-      setTotalDeposit(0)   
+      setTotalDeposit(0);
     }
   }, [connectedWallet, lcd]);
 
+  const deposit = async () =>{
+    const depositMsg = anchor.earn.depositStable({amount: '10', market: MARKET_DENOMS.UUSD})
+      .generateWithAddress(address)
+
+    const signResult = await connectedWallet?.sign({
+      msgs: depositMsg,
+    }) ;
+    console.log(signResult?.result);
+
+    const result = await lcd.tx.broadcast(signResult?.result as Tx);
+    console.log(result);
+
+  };
   
   return (
     <div>
       <h1>Anchor Earn</h1>
       <b>Total Deposts:</b> {totalDeposit} UST
+      <br />
+      <div><b>Depost</b> <button onClick={deposit}>Deposit</button></div>
     </div>
   );
 }
